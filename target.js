b3448fd4c4f4377a5592f4d2833692ad8ff7e268
For73testing <for73testing@gmail.com>
2026-09-08 18:31:05 +0530

    Remove GITHUB_TOKEN from target.js
    Removed sensitive GITHUB_TOKEN from configuration.

--- target.js
+++ target.js
@@ -1,5 +1,4 @@
 module.exports = {
-    GITHUB_TOKEN: "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",
     OWNER: "For73testing",
     REPO: "Automatic-fb-number-checking-",
     FILE_PATH: "number.txt"
