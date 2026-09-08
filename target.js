440b3bbd09e7df344f06fb570e7ff9d8814d4744
For73testing <for73testing@gmail.com>
2026-09-08 17:34:21 +0530

    Change GitHub token and repository information
    Updated GitHub repository configuration details.

--- target.js
+++ target.js
@@ -1,6 +1,6 @@
 module.exports = {
-    GITHUB_TOKEN: "Yahan_Target_Repo_Ka_Token_Aayega",
-    OWNER: "Target_Repo_Owner_Name",
-    REPO: "Target_Repo_Name",
+    GITHUB_TOKEN: "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",
+    OWNER: "For73testing",
+    REPO: "Automatic-fb-number-checking-",
     FILE_PATH: "number.txt"
 };
